5cc5fe1d9fc683a6d3ed7f798baeb0622d834780
Maya Friedrichs <mail@hyakosm.net>
2016-09-03 13:12:51 +0200

    Gestion des trous par tranches de 30 min

--- display.php
+++ display.php
@@ -38,7 +38,17 @@ function DivCoursOfDay($planning, $group, $timestamp, $dparms = null) {
 		$dparms = DisplayParams::getDefault();
 	}
 
+	$oldend = 8*60;
+	
 	foreach ($planning->getArrayCours($group, $timestamp) as $i) {
+	
+		$difference = $i->getStart()[3]*60+$i->getStart()[4] - $oldend;
+		
+		while($difference/30 > 0) {
+			echo '<div class="sep30"></div>';
+			$difference=$difference-30;
+		}
+		
 		$hue = crc32($i->getSubject() . $dparms->colorCrcSalt) % 360;
 		echo '<div class="cours" style="background:hsl('.$hue.', 100%, 90%)">';
 			echo '<h4 style="color:hsl('.$hue.', 100%, 40%)">'.$i->getSubject().'</h4>';
@@ -49,6 +59,8 @@ function DivCoursOfDay($planning, $group, $timestamp, $dparms = null) {
 					$i->getEnd()[3].':'.$i->getEnd()[4].'</p>';
 				echo '</div>';
 		echo "</div>";
+
+		$oldend = $i->getEnd()[3]*60+$i->getStart()[4];
 	}
 }
 
