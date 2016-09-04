9a232378265346de2c3fe9deb8395c0cc4aa225d
Celeste Wouters <elementw@openmailbox.org>
2016-09-04 18:46:59 +0200

    Planning.php: suppression de lignes/tab superflus

--- Planning.php
+++ Planning.php
@@ -1,9 +1,6 @@
 <?php
-
 require_once 'class.iCalReader.php';
 require_once 'Cours.php';
-
-
 
 class Planning {
 	private $ical;
@@ -11,43 +8,41 @@ class Planning {
 	private $events = [];
 	private $groups = [];
 	private $cours = [];
-	
+
 	/**
 	@param string $path Chemin ou URL du fichier ADE
 	@param integer $time_offset Décalage horaire (en heures)
 	*/
-
 	public function Planning($path, $time_offset=0) {
 		$this->ical = new ICal($path);
 		$this->events = $this->ical->events();
 		$this->time_offset = $time_offset;
-		
+
 		foreach ($this->events as $event) {
 			$item_cours = new Cours($event, $this->time_offset);
 			$this->cours[] = $item_cours;
 		}
-		
+
 		usort($this->cours, function($a, $b) {
 			return $a->getStart()[3]*60+$a->getStart()[4] >
 				   $b->getStart()[3]*60+$b->getStart()[4];
 		});
 	}
-	
+
 	/**
 	@param string $group = '' Filtrage possible par groupe
 	@param integer $timestamp Filtrage possible par jour via timestamp
 	@return array Liste d'objets de cours
 	*/
-	
 	public function getArrayCours($group='', $timestamp=null) {
 		$array_cours = [];
 		
 		$Y = date('Y', $timestamp);
 		$m = date('m', $timestamp);
 		$d = date('d', $timestamp);
-		
+
 		$array_date = array($Y, $m, $d);
-		
+
 		foreach ($this->cours as $i) {
 			if (($group == '')
 			|| in_array($group, $i->getGroups())) {
@@ -60,11 +55,10 @@ class Planning {
 		
 		return $array_cours;
 	}
-	
+
 	/**
 	@return array Liste des groupes ayant des cours
 	*/
-	
 	public function getArrayGroups() {
 		$groups = [];
 		
@@ -73,7 +67,7 @@ class Planning {
 				$groups[$group] = null;
 			}
 		}
-		
+
 		$groups = array_keys($groups);
 		sort($groups);
 		return $groups;
